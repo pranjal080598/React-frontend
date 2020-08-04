d849dd213d1f8fafdaaa416b5df73fa9fecb4fa2
pranjal080598 <pranjalnaringrekar08@gmail.com>
2020-08-04 23:03:21 +0530

    React-Router in Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
+import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 
@@ -10,28 +13,27 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
 
   render() {
-    return (
+      const HomePage = () => {
+      return(
+          <Home />
+      );
+      }
+      return(
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-      
-                  
-                     <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-                  
+        <Header />
+        <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              {/* when no specific url use redirect*/}
+              <Redirect to="/home" />
+          </Switch>
         
+        <Footer />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // add index.css later so it overrides default bootstrap css
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.css'; 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
