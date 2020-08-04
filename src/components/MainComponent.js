8873d246ca16fb005149ed774fba20e2feabc24f
pranjal080598 <pranjalnaringrekar08@gmail.com>
2020-08-05 01:09:16 +0530

    Added aboutus page and added link for same to maincomponent file

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -24,6 +26,7 @@ class Main extends Component {
 
 
   render() {
+
       const HomePage = () => {
       return(
            <Home 
@@ -33,6 +36,15 @@ class Main extends Component {
           />
       );
       }
+
+      //match, location and history are passed, we will only choose match
+      const DishWithId = ({match}) => {
+      return(
+        // convert string to base 10 int
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
       return(
       <div>
         <Header />
@@ -40,7 +52,9 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route exact path='/contactus' component={Contact} />
-              {/* when no specific url use redirect*/}
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} />
+              <Route path='/menu/:dishId' component={DishWithId} />
+              {/* // when no specific url use redirect */}
               <Redirect to="/home" />
           </Switch>
         
